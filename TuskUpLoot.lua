d513d25f87057be309266fb6a755f0703219b4f2
keithvsr <keithvsr@gmail.com>
2026-03-23 00:21:25 -0700

    reworked event handler to load item data

--- TuskUpLoot.lua
+++ TuskUpLoot.lua
@@ -22,16 +22,35 @@ function addon.isInRequiredGuild()
 end
 
 local eventFrame = CreateFrame("Frame")
-eventFrame:RegisterEvent("PLAYER_LOGIN")
+eventFrame:RegisterEvent("ADDON_LOADED")
+-- eventFrame:RegisterEvent("PLAYER_LOGIN")
 eventFrame:RegisterEvent("PLAYER_GUILD_UPDATE")
-eventFrame:SetScript("OnEvent", function()
-  if addon.DB and addon.DB.init then
-    addon.DB.init()
-  end
+eventFrame:RegisterEvent("ITEM_DATA_LOAD_RESULT")
+eventFrame:SetScript("OnEvent", function(_, event, ...)
+  if event == "ADDON_LOADED" then
+    local addonName = ...
+    if addonName ~= addon.addonName then
+      return
+    end
 
-  if not addon.isInitialized then
-    addon.isInitialized = true
-    addon.chatPrint("Initialized.")
+    -- only need to init the DB once on startup
+    if addon.DB and addon.DB.init then
+      addon.DB.init()
+    end
+
+    -- request to load item data for each item in DB
+    local itemIds = addon.DB.sortedItemIDs()
+    addon.totalItems = itemIds and #itemIds or 0
+    addon.pendingItems = {}
+    -- track the items that are pending
+    for _, itemId in ipairs(itemIds) do
+      addon.pendingItems[itemId] = true
+    end
+    -- request the item data
+    addon.chatPrint("AddOn Initialized. Requesting item data for " .. tostring(addon.totalItems) .. " items.")
+    for _, itemId in ipairs(itemIds) do
+      C_Item.RequestLoadItemDataByID(itemId)
+    end
 
     SLASH_TUSKUPLOOT1 = "/tul"
     SLASH_TUSKUPLOOT2 = "/tuskup"
@@ -40,9 +59,24 @@ eventFrame:SetScript("OnEvent", function()
         addon.UI:Toggle()
       end
     end
-  end
 
-  if addon.UI and addon.UI.frame and addon.UI.frame:IsShown() and not addon.isInRequiredGuild() then
-    addon.UI.frame:Hide()
+    eventFrame:UnregisterEvent("ADDON_LOADED")
+    -- end ADDON_LOADED event handler
+  elseif event == "PLAYER_GUILD_UPDATE" then
+    if addon.UI
+        and addon.UI.frame
+        and addon.UI.frame:IsShown()
+        and not addon.isInRequiredGuild() then
+      addon.UI.frame:Hide()
+    end
+  elseif event == "ITEM_DATA_LOAD_RESULT" then
+    local itemId, success = ...
+    if addon.pendingItems and addon.pendingItems[itemId] then
+      addon.pendingItems[itemId] = nil
+      if next(addon.pendingItems) == nil then
+        addon.pendingItems = nil -- clean up
+        addon.chatPrint("All item data requests completed.")
+      end
+    end
   end
 end)
